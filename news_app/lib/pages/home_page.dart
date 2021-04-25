c594a6aaad91ce4cc259e012fdfaca16cabb11fb
vidal1101 <rodrigovidal278@gmail.com>
2021-04-24 21:38:34 -0600

    creacion de la app de noticias

---
+++ news_app/lib/pages/home_page.dart
@@ -0,0 +1,14 @@
+import 'package:flutter/material.dart';
+
+class Home_Page extends StatelessWidget {
+  const Home_Page({Key key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Home'),
+      ),
+    );
+  }
+}
