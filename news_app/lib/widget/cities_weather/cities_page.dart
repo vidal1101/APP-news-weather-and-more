e5def859010683caab09cca01b48d03cb3351324
vidal1101 <rodrigovidal278@gmail.com>
2022-05-24 18:52:23 -0600

    trabajando en la seccion de climas

---
+++ news_app/lib/widget/cities_weather/cities_page.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+
+class Cities extends StatefulWidget {
+
+  @override
+  State<Cities> createState() => _CitiesState();
+}
+
+class _CitiesState extends State<Cities> {
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
