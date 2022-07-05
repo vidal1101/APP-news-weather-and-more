9bed1a7d6f4c9f72ab8d190ae09055813a2044a7
vidal1101 <rodrigovidal278@gmail.com>
2022-07-05 17:32:11 -0600

    recreamos el splash screen pero personalizado y usando lottie

--- news_app/lib/widget/splahscreen.dart
+++ news_app/lib/widget/splahscreen.dart
@@ -1,8 +1,10 @@
 import 'package:flutter/material.dart';
-import 'package:animated_splash_screen/animated_splash_screen.dart';
-import 'package:news_app/pages/home_page.dart';
+import 'package:news_app/widget/MainHome/background_home.dart';
 import 'package:news_app/widget/MainHome/main_home_page.dart';
 
+import 'package:animated_splash_screen/animated_splash_screen.dart';
+import 'package:page_transition/page_transition.dart';
+import 'package:lottie/lottie.dart'; //https://lottiefiles.com/
 /**
  * splahscreen de la app
  */
@@ -16,12 +18,48 @@ class _SplahState extends State<Splah> {
   @override
   Widget build(BuildContext context) {
     return AnimatedSplashScreen(
-      splash: 'assets/icon/app-information.png',
+      splash: _bodySplash(),
+      //backgroundColor: Colors.red,
+      splashIconSize: double.infinity,
       nextScreen: MainSplashHome(), //mainHiome
-      duration: 2000,
-      centered: true,
-      curve: Curves.easeInCirc,
-      splashTransition: SplashTransition.rotationTransition,
+      duration: 3500,
+      //centered: true,
+      curve: Curves.easeInExpo,
+      splashTransition: SplashTransition.fadeTransition,
+      pageTransitionType: PageTransitionType.leftToRight, 
+      animationDuration: Duration(microseconds: 1700), 
+      );
+  }
+
+  Widget _bodySplash(){
+    final mediaquey = MediaQuery.of(context).size;
+    return Stack(
+      alignment: Alignment.center,
+      children: [
+
+        BackGroudHome(), // color y fonrmas de fondo
+
+        Column(
+        children: [
+        SizedBox(height: mediaquey.height * 0.30,),
+        Lottie.asset('assets/splash/splashloading2.json' ,
+        height: 200 , width: 200),
+        SizedBox(height: 10,),
+        Center(
+          child: Text('News,Movies and Weather',
+          style: TextStyle(fontSize: 15 , fontWeight:  FontWeight.bold, color:  Colors.white) ,),
+          ),
+        ],
+      ),
+
+      Positioned( 
+        top: mediaquey.height * 0.95,
+        child: Center(
+          child: Text('By Rodrigo Vidal' , 
+          style: TextStyle(color:  Colors.purpleAccent, fontSize: 20,   ) ),
+        )),
+      ],
     );
   }
+
 }
